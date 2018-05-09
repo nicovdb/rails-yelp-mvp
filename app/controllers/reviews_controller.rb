20ed7a27cb84b6fb1ac8c8ac0a2bab9049f5f755
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-05-09 14:19:22 +0200

    ex1done sans css

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params.require(:review).permit(:content, :rating, :restaurant_id))
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '06 13 27 01 81',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '06 22 22 27 77',
+    category:        'italian'
+  },
+  {
+    name:         'Paul',
+    address:      'rue Balguerie Bordeaux',
+    phone_number:  '06 13 27 01 81',
+    category:        'french'
+  },
+  {
+    name:         'Pizza nico',
+    address:      '112 rue Balguerie',
+    phone_number:  '06 22 22 27 77',
+    category:        'italian'
+  },
+  {
+    name:         'Carrefour',
+    address:      'cours du medoc',
+    phone_number:  '06 13 27 01 81',
+    category:        'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
